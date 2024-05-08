cefe7e5ebbce866a02903a3eadc512ee98668dcd
ChristopherPeralta <114698519+ChristopherPeralta@users.noreply.github.com>
2024-05-08 08:34:46 -0500

    init

---
+++ ReadJsonTest/ReadJsonTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadJsonTestApp.swift
+//  ReadJsonTest
+//
+//  Created by Christopher Peralta on 6/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ReadJsonTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
